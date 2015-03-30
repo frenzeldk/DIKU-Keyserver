650edc8f42ba1e461330cddf93b2f161d51e204e
DIKU-keys dev-server <dikukeys@dikukeys.dk>
2015-03-30 07:50:36 +0200

    Comments and changed back to only allow alumni.ku.dk e-mail addresses as recipients. Changed e-mail body back to being a variable.

--- app.go
+++ app.go
@@ -15,15 +15,16 @@ type FastCGIServer struct{}
 
 func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	resp.Write([]byte("<form>KU-ID:<br><input type='text' name='ku_id'><br><input type='submit' value='Send'></form>"))
-	rcpt := req.FormValue("ku_id")
-	//epost := strings.Join([]string{rcpt, "alumni.ku.dk"}, "@")
+	//kuid is the KU ID of the student
+	kuid := req.FormValue("ku_id")
+	//rcpt is the e-mail address associated with kuid
+	rcpt := strings.Join([]string{kuid, "alumni.ku.dk"}, "@")
+	//body is the plaintext body of the email.
+	body := `Dette er epostens krop.
+	         linjeskifte laves ved at have regulære linjeskift.`
+	//only send an email if rcpt has a value. This needs to be changed to regex for a valid e-email adress (user@domain.tld)
 	if rcpt != "" {
-		mail.Send(rcpt, `Kære liste,
-			denne epost er sendt igennem den golang-app,
-			som vi har brugt dagen i dag på at kode.
-			
-			Venligst
-			Thorkil, Mads & Sven`)
+		mail.Send(rcpt, body)
 	}
 	fmt.Println("A mail has been sent to:", rcpt)
 	fmt.Println(time.Now())
