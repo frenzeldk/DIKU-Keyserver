f350b401d4d1dade4d57def955b406e1046c59f3
Sven Frenzel <sven@frenzel.dk>
2015-04-23 16:44:18 +0200

    readd regex

--- app.go
+++ app.go
@@ -35,33 +35,30 @@ func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 
 	pubkey := req.FormValue("pubkey")
 
-	//rcpt is the e-mail address associated with kuid
-	//commented out until we no longer get caught be the office 365 spamfilter
-	//rcpt := kuid + "@alumni.ku.dk"
-	rcpt := kuid
+	rcpt := kuid + "@alumni.ku.dk"
+
 	fmt.Println(rcpt)
 
 	if kuid == "" {
-		t, _ := template.ParseFiles("/home/dikukeys/Orkeren/DIKU-Keyserver/html_templates/create_link.html")
+		t, _ := template.ParseFiles("/home/keys/Orkeren/DIKU-Keyserver/html_templates/create_link.html")
 		t.Execute(resp, p)
 		//resp.Write([]byte("<form>KU-ID:<br><input type='text' name='kuid'>@alumni.ku.dk<br><input type='submit' value='Send'></form>"))
 	} else if coffee_hash == "" {
 		ctime = strconv.FormatInt(time.Now().Unix(), 10)
-		coffee_hash = hex.EncodeToString(hash.GetHash(kuid, ctime)[:])
+		coffee_hash = hex.EncodeToString(hash.GetHash(kuid + ctime)[:])
 
 		//body is the plaintext body of the email.
-		body := `Velkommen til dikukeys. For at afslutte registreringen, tryk venligst på dette link:
+		body := 
+`Velkommen til dikukeys. For at afslutte registreringen, tryk venligst på dette link:
 http://dikukeys.dk:8081/app?kuid=` + kuid + "&ctime=" + ctime + "&hash=" + coffee_hash
-		//only send an email if rcpt has a value. This needs to be changed to regex for a valid e-email adress (user@domain.tld)
-		//again due to office 365 we currently only check if the field is empty or not instead of checking if the email is a ku-student email.
-		//if rcpt != "@alumni.ku.dk" {
-		if rcpt != "" {
+		
+		if rcpt != "@alumni.ku.dk" {
 			mail.Send(rcpt, body)
 		}
 		t, _ := template.ParseFiles("/home/dikukeys/Orkeren/DIKU-Keyserver/html_templates/reg_mail_sent.html")
 		t.Execute(resp, p)
 		//resp.Write([]byte("<p>Registration e-mail sent!</p>"))
-	} else if hex.EncodeToString(hash.GetHash(kuid, ctime)[:]) == coffee_hash {
+	} else if hex.EncodeToString(hash.GetHash(kuid + ctime)[:]) == coffee_hash {
 		t, _ := template.ParseFiles("/home/dikukeys/Orkeren/DIKU-Keyserver/html_templates/public_key.html")
 		t.Execute(resp, p)
 		//resp.Write([]byte("<form>public key:<br><input type='text' name='pubkey'><br><input type='submit' value='Send'></form>"))
--- golibs/hash/hash.go
+++ golibs/hash/hash.go
@@ -7,11 +7,11 @@ import (
 	//	"time"
 )
 
-func GetHash(str1, str2 string) (hash []byte) {
+func GetHash(str string) (hash []byte) {
 	h512 := sha512.New()
 	//	time := time.Now().Unix()        // We use Unix time format
 	//	t := strconv.FormatInt(time, 10) // We now change the Int into a String
 	secret := "this is our muuch secret string"
-	io.WriteString(h512, str1+str2+secret)
+	io.WriteString(h512, str + secret)
 	return h512.Sum(nil)
 }
