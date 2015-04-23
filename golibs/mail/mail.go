9bf3839cb29f91e3d34554bba8a192d841a40dff
Sven Frenzel <sven@frenzel.dk>
2015-04-23 16:12:05 +0200

    more email fixes

--- golibs/mail/mail.go
+++ golibs/mail/mail.go
@@ -7,10 +7,10 @@ import (
 )
 
 func Send(rcpt, body string) {
-		bla := "From: DIKUKeys <noreply@dikukeys.dk
-		" + "To: " + rcpt + "
+		bla := 'From: DIKUKeys <noreply@dikukeys.dk
+		' + "To: " + rcpt + '
 		Subject: Test
-		" + body
+		' + body
         // Connect to the remote SMTP server.
         c, err := smtp.Dial("dikukeys.dk:25")
         if err != nil {
