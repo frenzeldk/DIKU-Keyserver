41249a239800ea4dad12a386b401bc6406a43977
Mads Gram <mgmadsgram@gmail.com>
2015-05-10 05:37:21 +0200

    Changed mail.go to use small letters for variables and smtp server has been given a variable

--- golibs/mail/mail.go
+++ golibs/mail/mail.go
@@ -1,36 +1,37 @@
 package mail
 
 import (
-        "bytes"
-        "log"
-        "net/smtp"
+	"bytes"
+	"log"
+	"net/smtp"
 )
 
 func Send(rcpt, body string) {
-		From := `From: DIKU Keys <noreply@dikukeys.dk>
+	stmp_server := "dikukeys.dk:25"
+	from := `From: DIKU Keys <noreply@dikukeys.dk>
 `
-		To := "To: " + rcpt + `
+	to := "To: " + rcpt + `
 `
-		Subject := `Subject: Velkommen til DIKU Keys
+	subject := `Subject: Velkommen til DIKU Keys
 `
-		epost := From + To + Subject + body
-        // Connect to the remote SMTP server.
-        c, err := smtp.Dial("dikukeys.dk:25")
-        if err != nil {
-                log.Fatal(err)
-        }
-        // Set the sender and recipient.
-        c.Hello("dikukeys.dk")
-        c.Mail("noreply@dikukeys.dk")
-        c.Rcpt(rcpt)
-        // Send the email body.
-        wc, err := c.Data()
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer wc.Close()
-        buf := bytes.NewBufferString(epost)
-        if _, err = buf.WriteTo(wc); err != nil {
-                log.Fatal(err)
-        }
+	epost := from + to + subject + body
+	// Connect to the remote SMTP server.
+	c, err := smtp.Dial(stmp_server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Set the sender and recipient.
+	c.Hello("dikukeys.dk")
+	c.Mail("noreply@dikukeys.dk")
+	c.Rcpt(rcpt)
+	// Send the email body.
+	wc, err := c.Data()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer wc.Close()
+	buf := bytes.NewBufferString(epost)
+	if _, err = buf.WriteTo(wc); err != nil {
+		log.Fatal(err)
+	}
 }
