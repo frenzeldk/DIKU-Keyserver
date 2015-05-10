e16fabcbbd20d389c807e3b6c96c7b588744665d
Mads Gram <mgmadsgram@gmail.com>
2015-05-10 04:32:36 +0200

    Oleks was not right in kuid !=  could be left out

--- app.go
+++ app.go
@@ -41,7 +41,7 @@ func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 
 	pubkey := req.FormValue("pubkey")
 
-	if !validKUID(kuid) {
+	if !validKUID(kuid) && kuid != "" {
 		resp.Write([]byte("<p>Not a valid link!</p>"))
 	}
 	rcpt := kuid + "@alumni.ku.dk"
