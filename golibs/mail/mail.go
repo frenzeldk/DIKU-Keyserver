58de68af3905c244f01353f3df1b72dbdaeca107
Mads Gram <mgmadsgram@gmail.com>
2015-05-10 06:01:05 +0200

    Trying to fix å issue in mail

--- golibs/mail/mail.go
+++ golibs/mail/mail.go
@@ -8,13 +8,12 @@ import (
 
 func Send(rcpt, body string) {
 	stmp_server := "dikukeys.dk:25"
-	from := `From: DIKU Keys <noreply@dikukeys.dk>
-`
-	to := "To: " + rcpt + `
-`
-	subject := `Subject: Velkommen til DIKU Keys
-`
-	epost := from + to + subject + body
+	from := `From: DIKU Keys <noreply@dikukeys.dk>`
+	to := "To: " + rcpt + ``
+	subject := `Subject: Velkommen til DIKU Keys`
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+
+	epost := from + to + subject + mime + body
 	// Connect to the remote SMTP server.
 	c, err := smtp.Dial(stmp_server)
 	if err != nil {
