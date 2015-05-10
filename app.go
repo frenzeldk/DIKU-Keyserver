d69447f52d0948879752ec6ccc2b625069214b89
Mads Gram <mgmadsgram@gmail.com>
2015-05-10 04:58:46 +0200

    commented the fmt import, since fmt.println is no longer used

--- app.go
+++ app.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/hex"
-	"fmt"
+	//"fmt"
 	"github.com/Orkeren/DIKU-Keyserver/golibs/hash" // This is our hash function
 	"github.com/Orkeren/DIKU-Keyserver/golibs/mail" // This is our mail function, it does hello
 	//	"github.com/Orkeren/DIKU-Keyserver/golibs/dbs" // This is our sqlite function
